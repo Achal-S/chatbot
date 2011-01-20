cba794dd4d57232051981bc6a236a311b585d08f
zerone <zerone@zerone-virtual-machine.(none)>
2011-01-20 14:36:51 +0000

    add java files only

---
+++ src/chatbot/BotBrain.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chatbot;
+
+/**
+ *
+ * @author zerone
+ */
+public class BotBrain {
+
+    public String speak(String message){
+        return message;
+    }
+
+}
